ffec5c2255453bee7dab76068201e1d7c9f628d5
RattyDAVE <dave@pucknell.co.uk>
2019-12-12 21:50:39 +0000

    Update init.sh

--- scripts/sys/init.sh
+++ scripts/sys/init.sh
@@ -4,4 +4,15 @@ export M2_HOME=/usr/share/maven
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 export PATH=$PATH:$JAVA_HOME/bin
 
+mkdir -p ~/.jupyter
+mkdir -p ~/.local/share/jupyter/runtime
+
+codemirror_dir=~/.jupyter/lab/user-settings/@jupyterlab/codemirror-extension
+mkdir -p $codemirror_dir
+cp /settings/commands.jupyterlab-settings $codemirror_dir
+
+apputils_dir=~/.jupyter/lab/user-settings/@jupyterlab/apputils-extension/
+mkdir -p $apputils_dir
+cp /settings/themes.jupyterlab-settings $apputils_dir
+
 source /scripts/sys/launch.sh 
