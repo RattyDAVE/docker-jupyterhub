71ab13bfcfd409a6d37f7a2e3bc292ffa300872c
RattyDAVE <dave@pucknell.co.uk>
2019-12-12 21:33:01 +0000

    Update init.sh

--- scripts/sys/init.sh
+++ scripts/sys/init.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 
 # create a user
-DOCKER_USER=${DOCKER_USER:-dclong} 
-DOCKER_USER_ID=${DOCKER_USER_ID:-9001} 
-DOCKER_PASSWORD=${DOCKER_PASSWORD:-$DOCKER_USER} 
-DOCKER_GROUP=${DOCKER_GROUP:-docker} 
-DOCKER_GROUP_ID=${DOCKER_GROUP_ID:-9001}
-/scripts/sys/create_user.sh $DOCKER_USER $DOCKER_USER_ID $DOCKER_PASSWORD $DOCKER_GROUP $DOCKER_GROUP_ID
-gpasswd -a $DOCKER_USER sudo
 
 /scripts/sys/etc.sh pre
 
