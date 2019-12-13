9646f5694b4fd7f9ba9ee58d5f312b10cbbdf7b3
RattyDAVE <dave@pucknell.co.uk>
2019-12-13 08:45:03 +0000

    Update init.sh

--- scripts/sys/init.sh
+++ scripts/sys/init.sh
@@ -1,5 +1,51 @@
 #!/usr/bin/env bash
 
+#Set Timezone
+if [[ -z "${TZ}" ]]; then
+   ln -fs /usr/share/zoneinfo/Europe/London /etc/localtime
+   dpkg-reconfigure -f noninteractive tzdata
+else
+   ln -fs /usr/share/zoneinfo/${TZ} /etc/localtime
+   dpkg-reconfigure -f noninteractive tzdata
+fi
+
+#CREATE USERS.
+# username:passsword:Y
+# username2:password2:Y
+
+file="/root/createusers.txt"
+if [ -f $file ]
+  then
+    while IFS=: read -r username password is_sudo
+        do
+            echo "Username: $username, Password: $password , Sudo: $is_sudo"
+
+            if getent passwd $username > /dev/null 2>&1
+              then
+                echo "User Exists"
+              else
+                useradd -ms /bin/bash $username
+                usermod -aG audio $username
+                usermod -aG input $username
+                usermod -aG video $username
+                mkdir -p /run/user/$(id -u $username)/dbus-1/
+                chmod -R 700 /run/user/$(id -u $username)/
+                chown -R "$username" /run/user/$(id -u $username)/
+                echo "$username:$password" | chpasswd
+                if [ "$is_sudo" = "Y" ]
+                  then
+                    usermod -aG sudo $username
+                fi
+            fi
+    done <"$file"
+fi
+
+startfile="/root/startup.sh"
+if [ -f $startfile ]
+  then
+    sh $startfile
+fi
+
 export M2_HOME=/usr/share/maven
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 export PATH=$PATH:$JAVA_HOME/bin
